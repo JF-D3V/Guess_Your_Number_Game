c554f12d4769ca2b640dc0caec717359ad43c242
JF-D3V <james.devdesign@gmail.com>
2023-11-29 15:39:23 +0200

    JS5_29.11

--- script.js
+++ script.js
@@ -10,7 +10,6 @@
 // console.log(document.querySelector(`.guess`).value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(`.number`).textContent = secretNumber;
 
 let score = 20;
 
@@ -18,10 +17,21 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess);
 
+  // When there is no number input
   if (!guess) {
     document.querySelector(`.message`).textContent = `No Number`;
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `Correct Number!`;
+
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+
+    document.querySelector(`.number`).style.width = `30rem`;
+
+    document.querySelector(`.number`).textContent = secretNumber;
+
+    // If input is more than
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `Too high!`;
@@ -31,6 +41,8 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.message`).textContent = `You Lose!`;
       document.querySelector(`.score`).textContent = 0;
     }
+
+    // If input is less than
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `Too low!`;
@@ -41,4 +53,13 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.score`).textContent = 0;
     }
   }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.number`).style.width = `15rem`;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`.score`).textContent = 20;
 });
